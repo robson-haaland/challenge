51a1da21444b8e616325c38407351a34aad8b655
robson-haaland <132105531+robson-haaland@users.noreply.github.com>
2023-07-03 19:16:48 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true
+});
