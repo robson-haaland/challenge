047b4669bc79db343c8497cbc96f9995a5107f25
robson-haaland <132105531+robson-haaland@users.noreply.github.com>
2023-07-25 16:15:51 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -16,19 +16,4 @@ const swiper = new Swiper('.swiper', {
     loop: true
 });
 
-// A modal em si
-const modal = document.querySelector(".modal");
 
-// O botão de Abrir Modal
-const openModalBtn = document.querySelector(".open");
-
-// O botão de Fechar Modal
-const closeModalBtn = document.querySelector(".close");
-
-/* Ao clicar no botão de Abrir Modal, executamos a função
-showModal() na Modal: */
-openModalBtn.addEventListener("click", () => modal.showModal());
-
-/* Ao clicar no botão de Fechar Modal, executamos a função
-close() na Modal */
-closeModalBtn.addEventListener("click", () => modal.close());
